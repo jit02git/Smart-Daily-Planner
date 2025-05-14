fd19edae3b0e6c7fc16622dae25c15c470eabf74
jitesh <jitesh02sep@gmail.com>
2025-05-15 00:51:51 +0530

    created the schema

---
+++ server/Model/Task.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const TaskSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String },
+  dueDate: { type: String, required: true }, 
+  dueTime: { type: String },
+  repeat: { type: Boolean, default: false },
+  priority: {
+    type: String,
+    enum: ["Low", "Medium", "High"],
+    default: "Medium",
+  },
+  category: { type: String },
+  reminder: { type: String },
+  status: { type: Boolean, default: false },
+});
+
+const Task = mongoose.model("Task", TaskSchema);
+
+export default Task;
