d0606891c6a47064f3e81974468e019cb6563a14
jitesh <jitesh02sep@gmail.com>
2025-07-08 22:07:09 +0530

    fix minor changes

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,11 +1,24 @@
-import TaskForm from "./Components/Form";
-import List from "./Components/List"; // Import your List component
+// pages/index.jsx
+import React, { useState } from "react";
+import Form from "./Components/Form";
+import List from "./Components/List";
 
 export default function Home() {
+  const [showModal, setShowModal] = useState(false);
+
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
-      <TaskForm />
-      <List /> {/* Render the list of tasks */}
+    <div className="min-h-screen bg-gray-100 p-6 flex flex-col items-center">
+
+      <button
+        onClick={() => setShowModal(true)}
+        className="bg-blue-600 text-white px-6 py-2 rounded-lg hover:bg-blue-700 transition mb-4"
+      >
+        ➕ Add Task
+      </button>
+
+      <Form show={showModal} onClose={() => setShowModal(false)} />
+
+      <List />
     </div>
   );
 }
