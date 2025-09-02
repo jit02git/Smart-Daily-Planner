d3f21a8e2b029bfa946c5b38cb9ed259edc5c37c
jitesh <jitesh02sep@gmail.com>
2025-09-02 20:58:57 +0530

    route fix

--- server/Route/TaskRoute.js
+++ server/Route/TaskRoute.js
@@ -5,8 +5,8 @@ const router = express.Router();
 
 router.post('/create', TaskController.createTask);
 router.get('/list', TaskController.getTaskList);
-router.post('view/:id', TaskController.getTaskById);
-router.post('delete/:id', TaskController.deleteTask);
-router.post('update/:id', TaskController.updateTask);
+router.post('/view/:id', TaskController.getTaskById);
+router.post('/delete/:id', TaskController.deleteTask);
+router.post('/update/:id', TaskController.updateTask);
 
 module.exports = router;
